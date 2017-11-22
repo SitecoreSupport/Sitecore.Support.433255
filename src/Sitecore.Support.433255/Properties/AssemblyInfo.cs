d81a6f71f7339bac5adf6e2fba82ffc0754afba2
AntonKechashin <atk@sitecore.net>
2017-11-22 17:08:17 +0200

    Initial commit

---
+++ src/Sitecore.Support.433255/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.433255")]
+[assembly: AssemblyProduct("Sitecore.Support.433255")]
+[assembly: ComVisible(false)]
